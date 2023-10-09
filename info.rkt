18365623aad404b13421c2f653f4223a70fccde0
a11ce <a11ce@protonmail.com>
2023-10-09 13:32:11 -0500

    Add to Examples doc category

--- info.rkt
+++ info.rkt
@@ -2,9 +2,8 @@
 
 (define collection "scribble-cheatsheet")
 
-(define scribblings '(("scribble-cheatsheet.scrbl")))
+(define scribblings '(("scribble-cheatsheet.scrbl" () ("Examples"))))
 
 (define build-deps '("scribble-lib"))
 
-(define license
-  '(Apache-2.0 OR MIT))
+(define license '(Apache-2.0 OR MIT))
